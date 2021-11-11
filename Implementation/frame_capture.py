4791c634785f803ad810f9a2b3579aff7f169fa0
liamh1999 <liam_hoang1999@hotmail.com>
2021-11-11 12:10:25 +0100

    added frame capture

---
+++ Implementation/frame_capture.py
@@ -0,0 +1,29 @@
+import threading
+from cv2 import cv2
+import time
+
+
+# https://stackoverflow.com/questions/8600161/executing-periodic-actions
+def do_every(period, f, *args):
+    def g_tick():
+        t = time.time()
+        while True:
+            t += period
+            yield max(t - time.time(), 0)
+
+    g = g_tick()
+    while True:
+        time.sleep(next(g))
+        f(*args)
+
+
+camera = cv2.VideoCapture(0)  # if 0 does not work try -1,0,1,2,...,
+
+
+def capture_frame():
+    return_value, frame = camera.read()
+    cv2.imshow("Laptop Camera Image", frame)  # testing purpose
+    do_every(10, capture_frame)
+    return frame
+
+# camera.release()
