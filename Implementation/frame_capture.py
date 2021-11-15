a07074cc5ca9b8879b6a197554d4952ec332ca2c
Daniel <daniel.gottschling1024@gmail.com>
2021-11-15 20:32:39 +0100

    capure_frame function is working

--- Implementation/frame_capture.py
+++ Implementation/frame_capture.py
@@ -16,14 +16,15 @@ def do_every(period, f, *args):
         time.sleep(next(g))
         f(*args)
 
-
 camera = cv2.VideoCapture(0)  # if 0 does not work try -1,0,1,2,...,
-
 
 def capture_frame():
     return_value, frame = camera.read()
-    cv2.imshow("Laptop Camera Image", frame)  # testing purpose
+    print("A picture was taken")
+    #cv2.imshow("Laptop Camera Image", frame)  # testing purpose
+    #cv2.waitKey(0)
     do_every(10, capture_frame)
     return frame
 
+capture_frame()
 # camera.release()
