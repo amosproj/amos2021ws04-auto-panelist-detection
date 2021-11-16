46988d1b8ce23cfc15a0e238753fbebdd886c9b6
liamh1999 <liam_hoang1999@hotmail.com>
2021-11-16 11:04:27 +0100

    Refactored Code

--- Implementation/frame_capture.py
+++ Implementation/frame_capture.py
@@ -1,4 +1,3 @@
-import threading
 from cv2 import cv2
 import time
 
@@ -16,15 +15,13 @@ def do_every(period, f, *args):
         time.sleep(next(g))
         f(*args)
 
-camera = cv2.VideoCapture(0)  # if 0 does not work try -1,0,1,2,...,
-
 def capture_frame():
+    camera = cv2.VideoCapture(0)  # if 0 does not work try -1,0,1,2,...,
     return_value, frame = camera.read()
     print("A picture was taken")
     #cv2.imshow("Laptop Camera Image", frame)  # testing purpose
     #cv2.waitKey(0)
-    do_every(10, capture_frame)
+    #do_every(10, capture_frame)
     return frame
 
-capture_frame()
-# camera.release()
+#camera.release() # We have to release the camera after we are done
