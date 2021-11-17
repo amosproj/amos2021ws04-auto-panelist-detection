cf27b3b6903a529b96747c3868b2b91ac6c127a3
Muhammad <m.elhwawshy@gmail.com>
2021-11-17 15:57:17 +0100

    refactor remote

--- Implementation/main.py
+++ Implementation/main.py
@@ -1,4 +1,4 @@
-import detection.remote as remote
+from detection.remote import Remote
 import detection.detection as detection
 
 import sys
@@ -8,6 +8,7 @@ import cv2
 
 
 def main():
+    remote = Remote()
     remote.login()
     while True:
         img = cv2.imread("C:\\Users\\iyadh\\Desktop\\kit.jpg")
@@ -18,6 +19,7 @@ def main():
         print('Currently active users: {}'.format(remote.get_logged_in()))
         if num_detected > len(remote.get_logged_in()):
             print('More people have been detected than are registered. Please log in.')
+            remote.login()
         time.sleep(10)
 
 
