f9390e44c29a50041905755b8c87ede3dcf4c19e
AirTrake <airtrake@gmail.com>
2021-11-25 02:18:46 +0100

    index funktioniert nun + e-mail wird gespeichert

--- Implementation/addfamilyentry.py
+++ Implementation/addfamilyentry.py
@@ -3,14 +3,14 @@ import pandas as pd
 col_names = ['ID', 'First Name', 'Last Name', 'Age', 'Gender', 'Photo']
 
 
-def addfamilyentry(ID, fName, lName, age, gender, photo):
+def addfamilyentry(fName, lName, age, gender, photo, email):
     old_df = pd.read_csv('family.csv')
 
-    new_data = {'ID': [ID], 'First Name': fName, 'Last Name': lName, 'Age': age, 'Gender': gender,
-                'Photo': photo}
+    new_data = {'ID': [(old_df.iloc[-1][0]+1)], 'First Name': fName, 'Last Name': lName, 'Age': age, 'Gender': gender,
+                'Photo': photo, 'E-Mail': email}
 
     new_df = pd.DataFrame(new_data)
-
     m_df = old_df.append(new_df)
-    m_df.to_csv("family.csv", index = False)
+    print(m_df)
+    m_df.to_csv("family.csv", index=False)
 
