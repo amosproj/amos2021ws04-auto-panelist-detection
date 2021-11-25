62b6ace6cd97d236673cde14342dfc4b201baadc
AirTrake <airtrake@gmail.com>
2021-11-25 02:19:38 +0100

    um e-mail erweitert

--- Implementation/createFamilyCSV.py
+++ Implementation/createFamilyCSV.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import numpy as np
 
 
 col_names = ['First Name', 'Last Name', 'Age', 'Gender', 'Photo']
 
-data_1 = {'ID': [0], 'First Name': "Max", 'Last Name': "Mustermann", 'Age': 30, 'Gender': 'm', 'Photo': "empty"}
+data_1 = {'ID': [0], 'First Name': "Max", 'Last Name': "Mustermann", 'Age': 30, 'Gender': 'm', 'Photo': "/home/airtrake/Bilder/family.jpeg", 'E-Mail': "musterman@hotmail.de"}
 
 family = pd.DataFrame(data_1)
 
