c09d2ae96a833db60643d68ad60488edbb642605
muhammad <m.elhwawshy@gmail.com>
2022-01-26 20:24:14 +0100

    stats end point

---
+++ Implementation/api/stats.py
@@ -0,0 +1,16 @@
+import csv
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.get("/stats")
+def get_stats():
+    stats = []
+    with open("Implementation/logs.csv", newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            stats.append({'timestamp': row['timestamp'], 'gender': row['gender']})
+
+    return jsonify(stats)
+
