10e5d532b0e72d0a715da18c63223c09048fff0b
muhammad <m.elhwawshy@gmail.com>
2022-01-27 13:05:32 +0100

    fix requirements and refined code

--- Implementation/api/stats.py
+++ Implementation/api/stats.py
@@ -10,7 +10,7 @@ def get_stats():
     with open("Implementation/logs.csv", newline='') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            stats.append({'timestamp': row['timestamp'], 'gender': row['gender']})
+            stats.append({'timestamp': row['timestamp'], 'gender': row['gender'], 'age': row['age'], 'emotion': row['emotion']})
 
     return jsonify(stats)
 
